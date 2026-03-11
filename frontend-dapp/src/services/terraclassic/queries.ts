f504771c76b7ca2fbafb1ec6ab1740973ad97207
Plastic Digits <plasticdigits@protonmail.com>
2026-03-11 16:02:15 +0900

    Fixes for testing

--- frontend-dapp/src/services/terraclassic/queries.ts
+++ frontend-dapp/src/services/terraclassic/queries.ts
@@ -2,7 +2,7 @@ import { TERRA_LCD_URL } from '@/utils/constants'
 
 export async function queryContract<T>(contractAddress: string, queryMsg: Record<string, unknown>): Promise<T> {
   const queryBase64 = btoa(JSON.stringify(queryMsg))
-  const url = `${TERRA_LCD_URL}/terra/wasm/v1beta1/contracts/${contractAddress}/store?query_msg=${queryBase64}`
+  const url = `${TERRA_LCD_URL}/cosmwasm/wasm/v1/contract/${contractAddress}/smart/${queryBase64}`
   const response = await fetch(url)
   if (!response.ok) {
     let errorDetail = `Query failed: ${response.status}`
@@ -19,5 +19,5 @@ export async function queryContract<T>(contractAddress: string, queryMsg: Record
     throw new Error(errorDetail)
   }
   const data = await response.json()
-  return JSON.parse(atob(data.query_result)) as T
+  return data.data as T
 }
